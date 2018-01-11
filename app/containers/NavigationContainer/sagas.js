beef6ac6cde73a9eebde8120025f0004c3299fd6
bryan huizi <bryanhuizi@gmail.com>
2018-01-11 12:21:20 -0800

    module 4: server data via Redux-Saga

--- app/containers/NavigationContainer/sagas.js
+++ app/containers/NavigationContainer/sagas.js
@@ -1,11 +1,30 @@
 // import { take, call, put, select } from 'redux-saga/effects';
+import { takeLatest } from 'redux-saga';
+import { call, put } from 'redux-saga/effects';
+import { REQUEST_TOPICS } from './constants';
+import { requestTopicsSucceeded, requestTopicFailed } from './actions';
+
+
+export function fetchTopicsFromServer() {
+  return fetch('http://localhost:3000/api/topics')
+  .then(response => response.json());
+}
+
+function* fetchTopics() {
+  try {
+    const topics = yield call(fetchTopicsFromServer);
+    yield put(requestTopicsSucceeded(topics));
+  } catch (e) {
+    yield put(requestTopicFailed(e.message));
+  }
+}
 
 // Individual exports for testing
-export function* defaultSaga() {
-  return;
+export function* fetchTopicsSaga() {
+  yield* takeLatest(REQUEST_TOPICS, fetchTopics);
 }
 
 // All sagas to be loaded
 export default [
-  defaultSaga,
+  fetchTopicsSaga,
 ];
