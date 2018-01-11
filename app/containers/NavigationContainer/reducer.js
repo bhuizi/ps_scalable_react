fd31dd99b3c1e6e9126eca6ae5f3cda648cfca10
bryan huizi <bryanhuizi@gmail.com>
2018-01-11 15:48:22 -0800

    module 5: storing user input

--- app/containers/NavigationContainer/reducer.js
+++ app/containers/NavigationContainer/reducer.js
@@ -6,30 +6,20 @@
 
 import { fromJS } from 'immutable';
 import {
-  REQUEST_TOPICS,
   REQUEST_TOPICS_SUCCEEDED,
-  REQUEST_TOPICS_FAILED,
+  SELECT_TOPIC,
 } from './constants';
 
 const initialState = fromJS({
-  topics: [
-    {
-      name: 'libraries',
-      description: 'links to useful open source libraries',
-    },
-    {
-      name: 'apps',
-      description: 'links to new and exciting apps',
-    },
-    {
-      name: 'news',
-      description: 'links to programming related new articles',
-    },
-  ],
+  topics: [],
 });
 
 function navigationContainerReducer(state = initialState, action) {
   switch (action.type) {
+    case REQUEST_TOPICS_SUCCEEDED:
+      return state.set('topics', action.topics);
+    case SELECT_TOPIC:
+      return state.set('selectedTopic', action.topic);
     default:
       return state;
   }
