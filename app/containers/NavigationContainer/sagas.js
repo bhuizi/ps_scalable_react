175fdf7c282f743d54141c396d94d88eb46424c2
bryan huizi <bryanhuizi@gmail.com>
2018-01-15 15:25:46 -0800

    adds / to fix location url

--- app/containers/NavigationContainer/sagas.js
+++ app/containers/NavigationContainer/sagas.js
@@ -21,7 +21,7 @@ function* fetchTopics() {
 }
 
 function* pushTopic(action) {
-  yield put(push(`topics/${action.topic.name}`));
+  yield put(push(`/topics/${action.topic.name}`));
 }
 
 export function* selectTopicSaga() {
