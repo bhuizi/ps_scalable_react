16e1d0d8748ed2a2a1d718cdca4e62840e2ace20
bryan huizi <bryanhuizi@gmail.com>
2018-01-17 11:44:47 -0800

    module 8: styles/validation Login

--- app/components/Login/index.js
+++ app/components/Login/index.js
@@ -5,14 +5,33 @@
 */
 
 import React from 'react';
-
+import validator from 'email-validator';
 import styles from './styles.css';
 
 class Login extends React.Component { // eslint-disable-line react/prefer-stateless-function
+  login = () => {
+    const email = this.emailField.value;
+    if (!validator.validate(email)) {
+      console.error('not a valid email');
+    }
+  }
   render() {
     return (
       <div className={styles.login}>
-        this is the login compoent
+        <div className={styles.heading}>
+          Login with your email
+        </div>
+        <input
+          className={styles.input}
+          type="text"
+          placeholder="your email"
+          ref={(f) => { this.emailField = f; }}
+        />
+        <div className={styles.actionContainer}>
+          <button className={styles.button}>
+          cancel</button>
+          <button className={styles.button} onClick={this.login}>login</button>
+        </div>
       </div>
     );
   }
