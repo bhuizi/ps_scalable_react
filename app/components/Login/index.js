42a81065b4322ff576cc63cd8e675584704e98c5
bryan huizi <bryanhuizi@gmail.com>
2018-01-17 15:54:23 -0800

    module 8: form values to state

--- app/components/Login/index.js
+++ app/components/Login/index.js
@@ -6,27 +6,47 @@
 
 import React from 'react';
 import validator from 'email-validator';
+import classNames from 'classnames';
 import styles from './styles.css';
 
 class Login extends React.Component { // eslint-disable-line react/prefer-stateless-function
+  static propTypes = {
+    login: React.PropTypes.func.isRequired,
+  }
+  state = {};
+
   login = () => {
     const email = this.emailField.value;
     if (!validator.validate(email)) {
-      console.error('not a valid email');
+      this.setState({
+        errorText: 'please provide a valid email',
+      });
+      return;
     }
+    this.setState({
+      errorText: null,
+    });
+    this.props.login(email);
   }
   render() {
+    const fieldError = this.state.errorText ? (
+      <div className={styles.errorMessage}>
+        {this.state.errorText}
+      </div>
+    ) : null;
+
     return (
       <div className={styles.login}>
         <div className={styles.heading}>
           Login with your email
         </div>
         <input
-          className={styles.input}
+          className={classNames(styles.input, { [styles.inputError]: this.state.errorText })}
           type="text"
           placeholder="your email"
           ref={(f) => { this.emailField = f; }}
         />
+        {fieldError}
         <div className={styles.actionContainer}>
           <button className={styles.button}>
           cancel</button>
